aa29e7c135086adffeb302240efcdb1ecf997cc1
gestagene <gestagenebrian@gmail.com>
2026-06-09 10:43:31 +0800

    feat(ui): add feed loading skeleton

---
+++ src/components/FeedSkeleton.tsx
@@ -0,0 +1,23 @@
+export default function FeedSkeleton() {
+  return (
+    <>
+      <div className="relative sm:mx-6 sm: my-2">
+        <div className="relative sm:w-175 text-justify px-2 pb-2 bg-gray-100 rounded-lg">
+          <div className="animate-pulse flex items-center space-x-3 px-2 pt-2 w-full">
+            <span className="rounded-4xl w-8 h-8 bg-gray-200/75"></span>
+            <span className="rounded-sm w-16 h-3 bg-gray-200/75"></span>
+            <span className="rounded-sm w-16 h-3 bg-gray-200/75"></span>
+            <span className="rounded-sm w-1/4 h-3 bg-gray-200/75"></span>
+          </div>
+          <div className="px-2 py-1 text-sm w-full">
+            <div className="bg-gray-200/75 h-28 w-full animate-pulse rounded-md"></div>
+          </div>
+          <div className="animate-pulse flex space-x-2 px-2 py-1">
+            <div className="py-1 px-1 bg-gray-200/75 rounded-full h-2 w-15 "></div>
+            <div className="py-1 px-1 bg-gray-200/75 rounded-full h-2 w-8 "></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
