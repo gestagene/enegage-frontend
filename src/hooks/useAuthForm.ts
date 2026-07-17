ecc5e32fb9cfe5a8b01d8b1682b55da52d36b6d4
gestagene <gestagenebrian@gmail.com>
2026-07-17 23:18:53 +0800

    feat: add custom hook for auth form handling

---
+++ src/hooks/useAuthForm.ts
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { userLogin, googleLogin } from "@/services/authService";
+import { userSignup } from "@/services/authService";
+import { supabase } from "@/lib/supabaseClient";
+
+export function useAuthForm(onSuccess: () => void, _onClose: () => void) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleGoogleLogin(credentialResponse: any) {
+    setIsLoading(true);
+    try {
+      const data = await googleLogin(credentialResponse.credential);
+      await supabase.auth.setSession({
+        access_token: data.session.access_token,
+        refresh_token: data.session.refresh_token,
+      });
+      onSuccess();
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleLogin(e: React.FormEvent) {
+    setIsLoading(true);
+    e.preventDefault();
+    setError("");
+    try {
+      const data = await userLogin(email, password);
+      await supabase.auth.setSession({
+        access_token: data.session.access_token,
+        refresh_token: data.session.refresh_token,
+      });
+
+      onSuccess();
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleSignup(e: React.FormEvent) {
+    setIsLoading(true);
+    e.preventDefault();
+    setError("");
+    try {
+      await userSignup(email, password, username);
+      setSuccessMessage("Check your email to confirm your account.");
+      setEmail("");
+      setPassword("");
+      setUsername("");
+      setTimeout(() => setSuccessMessage(""), 7000);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function switchToSignUp() {
+    setShowSignUp(true);
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setError("");
+  }
+
+  function switchToLogin() {
+    setShowSignUp(false);
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setError("");
+  }
+
+  return {
+    username,
+    setUsername,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    error,
+    showSignUp,
+    successMessage,
+    handleLogin,
+    handleGoogleLogin,
+    handleSignup,
+    switchToSignUp,
+    switchToLogin,
+    isLoading,
+  };
+}
