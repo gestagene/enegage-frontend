42bdda08f44a3562e90d05583b7c1841760c35fd
gestagene <gestagenebrian@gmail.com>
2026-08-12 23:24:44 +0800

    feat: add vote interaction logic

--- src/components/ui/ActionBar.tsx
+++ src/components/ui/ActionBar.tsx
@@ -3,19 +3,60 @@ import { PiArrowFatUpLight } from "react-icons/pi";
 import { BsArrowRepeat } from "react-icons/bs";
 import { PiShareFat } from "react-icons/pi";
 import { PiArrowFatDownLight } from "react-icons/pi";
+import { PiArrowFatUpFill } from "react-icons/pi";
+import { PiArrowFatDownFill } from "react-icons/pi";
+import type { vote } from "@/types/post";
+type ActionBarProps = {
+  voteScore?: number;
+  vote: vote | null;
+  handleVote: (v: vote) => void;
+};
 
-export default function ActionBar({ likes }: { likes?: string }) {
+export default function ActionBar({
+  voteScore,
+  vote,
+  handleVote,
+}: ActionBarProps) {
   return (
     <div className="flex space-x-2 items-center">
-      <button className="flex gap-1  justify-center items-center bg-gray-200/75 rounded-full w-18 text-xs">
-        <span className="p-1 hover:text-green-800 hover:bg-gray-300 rounded-full hover:cursor-pointer">
-          <PiArrowFatUpLight size={20} />
-        </span>
-        <span>{likes}</span>
-        <span className="p-1 hover:text-blue-800 hover:bg-gray-300 rounded-full hover:cursor-pointer">
-          <PiArrowFatDownLight size={20} />
-        </span>
-      </button>
+      <div
+        className={`flex justify-between items-center rounded-full w-auto ${
+          vote === "up"
+            ? "bg-green-800 text-white"
+            : vote === "down"
+              ? "bg-blue-800 text-white"
+              : "bg-gray-200"
+        }`}
+      >
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            handleVote("up");
+          }}
+          className={`${vote === "up" ? "hover:bg-green-900" : vote === "down" ? "hover:bg-blue-900" : "hover:bg-gray-300"} rounded-full py-1 px-2`}
+        >
+          {vote === "up" ? (
+            <PiArrowFatUpFill size={20} />
+          ) : (
+            <PiArrowFatUpLight size={20} />
+          )}
+        </button>
+        <span className="text-xs">{voteScore}</span>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            handleVote("up");
+          }}
+          className={`${vote === "up" ? "hover:bg-green-900" : vote === "down" ? "hover:bg-blue-900" : "hover:bg-gray-300"} rounded-full py-1 px-2`}
+        >
+          {vote === "down" ? (
+            <PiArrowFatDownFill size={20} />
+          ) : (
+            <PiArrowFatDownLight size={20} />
+          )}
+        </button>
+      </div>
+
       <button className="flex space-x-1  justify-center hover:bg-gray-300 hover:cursor-pointer items-center py-1 px-1 bg-gray-200/75 rounded-full w-8 text-xs text-black">
         <span>
           <PiChatCircleLight size={20} />
